780c6c0b9c14f1ab93b96d9a71ddec0c5eff5316
sheikhtaha98 <shiekhtahahameed1998@gmail.com>
2023-06-26 01:37:33 +0500

    list of string then convert into reversed mode

---
+++ q12.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> names = [
+    'Taha',
+    'Muqees ',
+    'Hayyan',
+    'Tulaib',
+    'Ahsan',
+    'Uzair',
+    'Zuhair'
+  ];
+  List<String> reversedNames = List.of(names.reversed);
+  print('original List: $names');
+  print('Reverse Order: $reversedNames');
+}
