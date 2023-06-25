0ed1923f94e0a563daf9151399fcbef37a04cdba
sheikhtaha98 <shiekhtahahameed1998@gmail.com>
2023-06-26 01:38:56 +0500

    list of int then sort in ascending order

---
+++ q14.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> orginalList = [6, 9, 13, 7, 0, 33, 47, 30, 48, 12, 71, 29];
+  print("Original List: $orginalList");
+  orginalList.sort();
+  List<int> sortedList = orginalList;
+  print('Sorted List: $sortedList');
+}
