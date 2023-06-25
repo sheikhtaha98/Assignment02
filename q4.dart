2fcd815367b7bf5812310eb0d2738c1326b438c7
sheikhtaha98 <shiekhtahahameed1998@gmail.com>
2023-06-26 01:23:04 +0500

    Map with name and keys

---
+++ q4.dart
@@ -0,0 +1,21 @@
+void main() {
+  List<int> numbers = [
+    13,
+    65,
+    77,
+    99,
+  ];
+  int smallest = numbers[0];
+  int greatest = numbers[0];
+
+  for (var y = 0; y < numbers.length; y++) {
+    if (numbers[y] < smallest) {
+      smallest = numbers[y];
+    }
+    if (numbers[y] > greatest) {
+      greatest = numbers[y];
+    }
+  }
+  print(smallest);
+  print(greatest);
+}
